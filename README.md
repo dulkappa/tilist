472c0446889e2308b5cdbac66c9652d0c02543b4
dulkappa <dulkappa@gmail.com>
2015-03-24 23:16:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# tilist
+a simple toggle switch application.
+
+- クリックするとタイルの背景と文字の濃淡が入れ替わる
+- ＋を押すことによってタイルを増やせる
+- 右クリックするとタイルを消去できる
+- ドラッグ＆ドロップで順番を変更できる
+
+## Development
+tilist requires to be installed gulp before running.
+To install gulp, run
+
+```
+npm install -g gulp
+```
+
+To install gulp module.
+
+```
+npm install
+```
+
+To install dependencies, use bower.
+
+```
+bower install
+```
+
+To run example server, run
+
+```
+gulp
+```
+
+## License
+
+Copyright(c)pantohon.com
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
+var webserver = require('gulp-webserver');
+
+gulp.task('jade', function(){
+	
+	gulp
+	.src('./templates/jade/index.jade')
+	.pipe(plumber())
+	.pipe(jade())
+	.pipe(gulp.dest('dist/'))
+	;
+
+});
+
+gulp.task('webserver', ['jade'], function(){
+
+	gulp
+	.src('./dist')
+	.pipe(
+		webserver({
+			port: 3333,
+			libereload: true
+		})
+	);
+	
+});
+
+gulp.task('default', ['webserver']);
